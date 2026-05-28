dd46b31db114df4ebe8b551366bcc604daf114da
Homura <egzozu.be.bas@gmail.com>
2026-05-28 19:37:41 +0300

    don't use custom libcxx on Android

--- source/angle/Build.hx
+++ source/angle/Build.hx
@@ -461,10 +461,7 @@ class Build
 	{
 		targetConfig.args.push('is_clang=true');
 		targetConfig.args.push('clang_use_chrome_plugins=false');
-		if (buildPlatform == 'android')
-        	targetConfig.args.push('use_custom_libcxx=true');
-    	else
-        	targetConfig.args.push('use_custom_libcxx=false');
+        targetConfig.args.push('use_custom_libcxx=false');
 
 		if (buildPlatform == 'ios')
 			targetConfig.args.push('ios_enable_code_signing=false');
